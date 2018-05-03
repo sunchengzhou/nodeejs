a25d79a3ed7df86a907e44334421c338b2266276
孙程洲 <sunchengzhou013@c07np11kg1j1.ljs.local>
2018-05-03 11:25:15 +0800

    new file

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 
 const indexRouter = require("./routers/index.js");
 const usersRouter = require("./routers/user.js");
+const answerRouter = require("./routers/answer.js");
 
 app .set("views", path.join(__dirname, "views"))
     .set("view engine", "ejs")
     .use("/",indexRouter)
     .use("/users", usersRouter)
+    .use("/answer",answerRouter)
     .listen(9000);
